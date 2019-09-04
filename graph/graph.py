1e2e41116f63899ef2bd1e4007e7d62a8041b1c3
Himanshu Singh <himanshux101@gmail.com>
2019-09-04 18:50:13 +0530

    clean graph module

--- graph/graph.py
+++ graph/graph.py
@@ -1,103 +1,72 @@
-from collections import defaultdict
+"""
+DFS uses stack 
+"""
+from graph import *
 
-class Graph:
-    def __init__(self):
-        self.graph = defaultdict(list)
+# number graph zero indexed 
+def DFSLoop(graph):
+    visited = [False] * (len(graph))
+    for u in graph:
+        DFS(graph, u, visited)
 
-    def addEdge(self, u, v):
-        self.graph[u].append(v)
+def DFS(graph, u, visited):
+    visited[u-1] = True
+    print(u)
+    for i in graph[u]:
+        if visited[i-1] == False:
+            DFS(graph, i, visited)
 
-    def generateEdges(self):
-        edges = []
-        for node in self.graph:
-            for neighbour in self.graph[node]:
-                edges.append((node, neighbour))
-        return edges
 
-    def returnGraph(self):
-        return self.graph
+"""
+BFS uses stack 
+"""
+from collections import defaultdict, deque
+from graph import *
 
-def visual(graph):
-  for i in graph:
-    for j in graph[i]:
-      print('{} -> {}'.format(i, j), end=' ')
-    print('')
+# number graph zero indexed
+def BFS(graph, s):
+    visited = [False] * (len(graph))
 
-def small():
-  g = Graph()
-  g.addEdge(0, 1)
-  g.addEdge(0, 2)
-  g.addEdge(1, 2)
-  g.addEdge(2, 0)
-  g.addEdge(2, 3)
-  g.addEdge(3, 3)
+    queue = deque()
+    queue.append(s)
+    visited[s] = True
 
-  return g.returnGraph()
+    while queue:
+        v = queue.popleft()
+        print(v)
 
-def big():
-  g = Graph()
-  g.addEdge(1, 3)
-  g.addEdge(1, 2)
-  g.addEdge(2, 1)
-  g.addEdge(2, 4)
-  g.addEdge(2, 5)
-  g.addEdge(3, 5)
-  g.addEdge(3, 1)
-  g.addEdge(4, 2)
-  g.addEdge(4, 5)
-  g.addEdge(4, 6)
-  g.addEdge(5, 6)
-  g.addEdge(5, 3)
-  g.addEdge(5, 2)
-  g.addEdge(5, 4)
-  g.addEdge(6, 4)
-  g.addEdge(6, 5)
+        for i in graph[v]:
+            if visited[i] == False:
+                visited[i] = True
+                queue.append(i)
 
-  return g.returnGraph()
+# s to v
+# graph number starts from 1 not 0
+def BFS_shortes_path(graph, s, v):
+    visited = [False] * (len(graph))
+    dist = defaultdict(int)
+    if s == v:
+        dist[v] = 0
+    else:
+        dist[v] = -99
 
-def scc_graph():
-  g = Graph()
-  g.addEdge(7, 1)
-  g.addEdge(4, 7)
-  g.addEdge(1, 4)
-  g.addEdge(9, 7)
-  g.addEdge(6, 9)
-  g.addEdge(3, 6)
-  g.addEdge(9, 3)
-  g.addEdge(8, 6)
-  g.addEdge(2, 8)
-  g.addEdge(5, 2)
-  g.addEdge(8, 5)
+    queue = deque()
+    queue.append(s)
+    visited[s - 1] = True
 
-  return g.returnGraph()
+    while queue:
+        v = queue.popleft()
+        print(v)
 
-def scc_graph_test1():
-    g = Graph()
-    g.addEdge(1, 4)
-    g.addEdge(2, 8)
-    g.addEdge(3, 7)
-    g.addEdge(3, 9)
-    g.addEdge(4, 4)
-    g.addEdge(4, 7)
-    g.addEdge(5, 2)
-    g.addEdge(6, 3)
-    g.addEdge(7, 1)
-    g.addEdge(8, 6)
-    g.addEdge(8, 20)
-    g.addEdge(9, 6)
-    g.addEdge(10, 1)
-    g.addEdge(10, 16)
-    g.addEdge(11, 10)
-    g.addEdge(12, 11)
-    g.addEdge(1, 15)
-    g.addEdge(13, 17)
-    g.addEdge(13, 18)
-    g.addEdge(14, 13)
-    g.addEdge(15, 17)
-    g.addEdge(16, 11)
-    g.addEdge(17, 12)
-    g.addEdge(18, 14)
-    g.addEdge(19, 5)
-    g.addEdge(20, 19)
+        for i in graph[v]:
+            print("i is {}".format(i))
+            if visited[i - 1] == False:
+                visited[i - 1] = True
+                dist[i] = dist[v] + 1
+                queue.append(i)
 
-    return g.returnGraph()
+    print("distance from {} to {} is {}".format(s, v, dist[v]))
+
+graph = big()
+print(len(graph))
+BFS_shortes_path(graph, 1, 4)
